776ce4b5aa8419cd8e9bbaf1435c4dd752b8cf3d
Clonetrash <jamison.bassett@gmx.de>
2020-10-08 15:45:37 +0100

    gotta figure out how triggers work, then can trans

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+    public Animator transition;
+    public string nextScene;
+    public float transitionTime = 1f;
+
+    void OnTriggerEnter2D (Collider2D collider) {
+            Debug.Log("Entered");
+            LoadNextLevel ();
+    }
+
+    public void LoadNextLevel () {
+        StartCoroutine (LoadLevel ());
+    }
+
+    IEnumerator LoadLevel () {
+        transition.SetTrigger ("start");
+        yield return new WaitForSeconds (transitionTime);
+        SceneManager.LoadScene (nextScene);
+    }
+
+}
