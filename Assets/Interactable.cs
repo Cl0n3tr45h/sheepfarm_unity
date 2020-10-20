eb68471210234854d010bc8057075eda2c99e608
Clonetrash <jamison.bassett@gmx.de>
2020-10-20 16:44:05 +0100

    this vorlesung so boring
    uhhhhhhhhhhhh made code prettiere(it is not prettier)

--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -2,42 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactable : MonoBehaviour
-{
+public class Interactable : MonoBehaviour {
 
     [SerializeField] string yarnStartNode;
     [SerializeField] YarnProgram yarnDialogue;
     public bool playerInRange = false;
-    
-    void Start()
-    {
-        Dialogue.instance.dialogueRunner.Add(yarnDialogue);   
+
+    void Start () {
+        Dialogue.instance.dialogueRunner.Add (yarnDialogue);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKey(KeyCode.Space) && playerInRange){
+    void Update () {
+        checkForInteraction ();
+    }
 
-            Player.isActive = false;
-            Dialogue.instance.dialogueRunner.StartDialogue(yarnStartNode);
-
+    void OnTriggerEnter2D (Collider2D otherCollider) {
+        if (otherCollider.CompareTag ("Player")) {
+            playerInRange = true;
         }
     }
 
-   
-
-    void OnTriggerEnter2D(Collider2D otherCollider)
-    {
-        if(otherCollider.CompareTag("Player")){
-            playerInRange = true;        
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D otherCollider)
-    {
-         if(otherCollider.CompareTag("Player")){
+    void OnTriggerExit2D (Collider2D otherCollider) {
+        if (otherCollider.CompareTag ("Player")) {
             playerInRange = false;
         }
+    }
+    void checkForInteraction () {
+        //if (Player.isActive == true) {
+            if (Input.GetKey (KeyCode.Space) && playerInRange) {
+
+                Player.isActive = false;
+                Dialogue.instance.dialogueRunner.StartDialogue (yarnStartNode);
+
+            }
+        //}
     }
 }
